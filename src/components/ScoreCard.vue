e8205b4e41c2b5ff22dfe9ef669697466872b721
Jason <howiejason93@gmail.com>
2024-08-02 20:37:16 -0400

    updates

--- src/components/ScoreCard.vue
+++ src/components/ScoreCard.vue
@@ -5,12 +5,53 @@ const props = defineProps<{ game: LiveGameDetails }>()
 
 <template>
   <div class="bg-slate-100 mt-10">
-    <table class="p-10">
-      <tr>
-        <th></th>
-        <th>1</th>
-        <th>2</th>
-      </tr>
+    <table class="w-full text-left">
+      <thead class="uppercase">
+        <th>&nbsp;</th>
+        <th>&nbsp;</th>
+        <th v-for="inning in game.liveData.linescore.innings" :key="inning.num">
+          {{ inning.num }}
+        </th>
+        <th>R</th>
+        <th>H</th>
+        <th>E</th>
+      </thead>
+      <tbody>
+        <tr>
+          <td>
+            <img
+              class="size-12 inline"
+              :src="`https://www.mlbstatic.com/team-logos/team-cap-on-light/${game.gameData.teams.away.id}.svg`"
+            />
+          </td>
+          <td>
+            {{ game.gameData.teams.away.name }}
+          </td>
+          <td v-for="inning in game.liveData.linescore.innings" :key="inning.num">
+            {{ game.liveData.linescore.innings[inning.num - 1]?.away.runs ?? 0 }}
+          </td>
+          <td>{{ game.liveData.linescore.teams.away.runs }}</td>
+          <td>{{ game.liveData.linescore.teams.away.hits }}</td>
+          <td>{{ game.liveData.linescore.teams.away.errors }}</td>
+        </tr>
+        <tr>
+          <td>
+            <img
+              class="size-12 inline"
+              :src="`https://www.mlbstatic.com/team-logos/team-cap-on-light/${game.gameData.teams.home.id}.svg`"
+            />
+          </td>
+          <td>
+            {{ game.gameData.teams.home.name }}
+          </td>
+          <td v-for="inning in game.liveData.linescore.innings" :key="inning.num">
+            {{ game.liveData.linescore.innings[inning.num - 1]?.away.runs ?? 0 }}
+          </td>
+          <td>{{ game.liveData.linescore.teams.away.runs }}</td>
+          <td>{{ game.liveData.linescore.teams.away.hits }}</td>
+          <td>{{ game.liveData.linescore.teams.away.errors }}</td>
+        </tr>
+      </tbody>
     </table>
   </div>
 </template>
--- src/types/LiveGameDetails.ts
+++ src/types/LiveGameDetails.ts
@@ -1,6 +1,45 @@
+export default interface Inning {
+  away: {
+    runs: number
+  }
+  home: {
+    runs: number
+  }
+  num: number
+}
+
 export default interface LiveGameDetails {
   gamePk: number
   gameData: {
     status: { abstractGameState: string }
+    teams: {
+      away: {
+        id: number
+        name: string
+      }
+      home: {
+        id: number
+        name: string
+      }
+    }
   }
+  liveData: {
+    linescore: {
+      innings: Array<Inning>
+      teams: {
+        away: {
+          runs: number
+          hits: number
+          errors: number
+        }
+        home: {
+          runs: number
+          hits: number
+          errors: number
+        }
+      }
+    }
+  }
+
+  //game.liveData.linescore.teams.away.runs
 }
